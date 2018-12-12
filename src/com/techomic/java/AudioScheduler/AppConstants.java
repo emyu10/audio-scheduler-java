4a3b9dff2b75ca2646f4268af1ae659e9651e426
Mohamed Usman <emyu10@gmail.com>
2018-12-12 23:47:27 +0500

    fixed DB_URL

--- src/com/techomic/java/AudioScheduler/AppConstants.java
+++ src/com/techomic/java/AudioScheduler/AppConstants.java
@@ -3,6 +3,6 @@ package com.techomic.java.AudioScheduler;
 public final class AppConstants {
     public final static String APP_TITLE = "Audio Scheduler";
     public final static String ERROR_APP_INIT_FAILED = "Could not initialize the application.";
-    public final static String DB_URL = "jdbc:sqlite:storage.db";
+    public final static String DB_URL = "jdbc:sqlite:" + AppConstants.class.getResource("storage.db").getPath();
     public final static String QUERY_SUCCESS = "QUERY_SUCCESS";
 }
